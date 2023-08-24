f04d0f2772fb1d698ec8e5c8b159bd070994bca4
Daniel-Dev3524 <daniel-lg@hotmail.com>
2023-08-23 22:13:11 -0300

    aula 05

---
+++ src/Componentes/Banner/index.js
@@ -0,0 +1,19 @@
+import './Banner.css'
+
+function Banner(){
+    return(
+
+        <header classNome="banner">
+
+            <img src="/imagens/banner_desi.png" alt="banner principal de formulário"/>
+
+        </header>
+        
+
+                
+
+
+    )
+}
+
+export default Banner;
